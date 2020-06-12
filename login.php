6a9b475312edf36f95b83c6a7a2a81963bfb8350
JoArce <jska2.1@gmail.com>
2020-06-12 00:19:05 -0400

    Add files login and style, and add images for login style

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+<LINK REL=StyleSheet HREF="style.css" TYPE="text/css" MEDIA=screen>
+</head>
+<center>
+
+  <form action=" curd.php" method = "POST">
+    <table>
+    <tr><td colpsan = "2"><label>Login</label></td></tr>
+    <tr><td aling = "center" rowspan ="5"> <img src="monobjj.png"></td><td><label> Usuario</label></td></tr>
+    <tr><td><input type="text" name = "txtusuario"></td></tr>
+    <tr><td><label> Password</label></td></tr>
+    <tr><td><input type="password" name = "txtpassword"></td></tr>
+    <tr><td><input type = "submit" value = "Iniciar Sesion"></td></tr>
+
+
+    
+    </table>
+  
+  </form>
+</center>
+</center>
+
+
+
+
+
+</html>
